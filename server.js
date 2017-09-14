3dace990a81ce0d99863ab53b7418d562564d41a
Nino Vrijman <nino-vrijman@users.noreply.github.com>
2017-09-14 10:47:05 +0200

    Disabled Arduino code

--- server.js
+++ server.js
@@ -29,35 +29,35 @@ io.on('connection', function (socket) {
   console.log('Socket: client connected');
 });
 
-const BOARD_COM_PORT = "COM5";
-const five = require('johnny-five');
-const board = new five.Board({
-  port: BOARD_COM_PORT
-});
+// const BOARD_COM_PORT = "COM5";
+// const five = require('johnny-five');
+// const board = new five.Board({
+//   port: BOARD_COM_PORT
+// });
 
-board.on('ready', function () {
-  console.log('Arduino: board ready!');
+// board.on('ready', function () {
+//   console.log('Arduino: board ready!');
 
-  let led = new five.Led(13);
-  let motion = new five.Motion({
-    pin: 2,
-    freq: 500
-  });
+//   let led = new five.Led(13);
+//   let motion = new five.Motion({
+//     pin: 2,
+//     freq: 500
+//   });
 
-  motion.on('calibrated', function () {
-    console.log('Motion sensor: calibrated');
-  });
+//   motion.on('calibrated', function () {
+//     console.log('Motion sensor: calibrated');
+//   });
 
-  motion.on('data', function (event) {
-    if (event.detectedMotion) {
-      led.on();
-      sendMessage('motion changed', { detectedMotion: true });
-    } else {
-      led.off();
-      sendMessage('motion changed', { detectedMotion: false });
-    }
-  });
-});
+//   motion.on('data', function (event) {
+//     if (event.detectedMotion) {
+//       led.on();
+//       sendMessage('motion changed', { detectedMotion: true });
+//     } else {
+//       led.off();
+//       sendMessage('motion changed', { detectedMotion: false });
+//     }
+//   });
+// });
 
 function sendMessage(event, data) {
   io.emit(event, data);
